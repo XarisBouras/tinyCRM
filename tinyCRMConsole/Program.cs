24adaa4c79763ace8655f39bbeb2bae87c72a251
Xaris Bouras <xarismpouras15@gmail.com>
2020-04-26 18:15:00 +0300

    Initiall commit

--- tinyCRMConsole/Program.cs
+++ tinyCRMConsole/Program.cs
@@ -6,74 +6,73 @@ namespace tinyCRMConsole
     {
         static void Main(string[] args)
         {
-            using System;
-            using System.Collections.Specialized;
-            using System.Threading;
 
-namespace teamDotnet_Day2
-    {
-        class Program
+             Console.WriteLine("Give your afm.");
+              Console.WriteLine(IsValidafm("88766"));
+            //   Console.WriteLine(IsValidafm("098765432"));
+            //  Console.WriteLine(IsValidafm("    0123547853  "));
+            //  Console.WriteLine(IsValidafm(null));
+            // var val = IsAdult(23);
+            // Console.WriteLine(val);
+            // Console.WriteLine(IsValidEmail("xaxa@cdcd.gr"));
+            var array = new int[4];
+            for(int i=0; i<array.Length; i++)
+            {
+                if (int.TryParse(Console.ReadLine(), out int num))
+                {
+                    array[i] = num;
+                 
+                }
+                else Console.WriteLine("Fail");
+            }
+            foreach(var i in array)
+            {
+                Console.WriteLine("Hey" + i);
+            }
+           
+        }
+        public static bool IsValidafm(string afm)
         {
-            static void Main(string[] args)
+            int number;
+            if (string.IsNullOrWhiteSpace(afm))
             {
-                Console.WriteLine("Give your afm.");
-                //  Console.WriteLine(IsValidafm("88766"));
-                //   Console.WriteLine(IsValidafm("098765432"));
-                //  Console.WriteLine(IsValidafm("    0123547853  "));
-                //  Console.WriteLine(IsValidafm(null));
-                // var val = IsAdult(23);
-                // Console.WriteLine(val);
-                Console.WriteLine(IsValidEmail("xaxa@cdcd.gr"));
-
-
-
-            }
-            public static bool IsValidafm(string afm)
-            {
-                int number;
-                if (string.IsNullOrWhiteSpace(afm))
-                {
-                    return false;
-                }
-
-                afm = afm.Trim();
-
-                if (afm.Length != 9)
-                {
-                    return false;
-                }
-
-                if (!int.TryParse(afm, out number))
-                {
-                    return false;
-                }
-
-                return true;
-            }
-
-            public static bool IsAdult(int age)
-            {
-                return age >= 18 && age <= 100;
-            }
-
-            public static bool IsValidEmail(string mail)
-            {
-                var s = 0;
-                if (!string.IsNullOrWhiteSpace(mail))
-                {
-                    mail = mail.Trim();
-                    for (int i = 0; i < mail.Length; i++)
-                    {
-                        if (mail[i] == '@') s++;
-                    }
-                    if (s == 1 && (mail.Contains(".gr") || mail.Contains(".com"))) return true;
-                }
                 return false;
             }
 
-        }
-    }
+            afm = afm.Trim();
 
-}
+            if (afm.Length != 9)
+            {
+                return false;
+            }
+
+            if (!int.TryParse(afm, out number))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsAdult(int age)
+        {
+            return age >= 18 && age <= 100;
+        }
+
+        public static bool IsValidEmail(string mail)
+        {
+            var s = 0;
+            if (!string.IsNullOrWhiteSpace(mail))
+            {
+                mail = mail.Trim();
+                for (int i = 0; i < mail.Length; i++)
+                {
+                    if (mail[i] == '@') s++;
+                }
+                if (s == 1 && (mail.Contains(".gr") || mail.Contains(".com"))) return true;
+            }
+            return false;
+        }
+
     }
 }
